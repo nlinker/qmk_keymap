29188a2ae40f4daca92526cd55b0b8fd07c83ec2
Nick Linker <nlinker@gmail.com>
2022-09-05 02:42:43 +0600

    add keys override

--- rules.mk
+++ rules.mk
@@ -2,11 +2,11 @@
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
-MOUSEKEY_ENABLE = no
 ORYX_ENABLE = yes
-TAP_DANCE_ENABLE = yes
 STENO_ENABLE = yes
 KEYBOARD_SHARED_EP = yes
 SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 SRC = matrix.c
