5c22b3ae9794214f19586a50b01e504ebd5fa14d
Nick Linker <nlinker@gmail.com>
2022-09-16 12:56:21 +0600

    add num layer to Iris, fix some problems in ML

--- iris/keymap.c
+++ iris/keymap.c
@@ -9,6 +9,7 @@ enum iris_layers {
   _RUS,
   _NAV,
   _SYM,
+  _NUM,
 };
 
 enum custom_keycodes {
@@ -25,6 +26,7 @@ enum tap_dance_codes {
 #define L_RUS  TG(_RUS)
 #define L_NAV  TT(_NAV)
 #define L_SYM  MO(_SYM)
+#define L_NUM  TG(_NUM)
 
 #define L_LSYM  LT(3, KC_X)
 #define M_LALT  MT(MOD_LALT, KC_C)
@@ -40,13 +42,13 @@ enum tap_dance_codes {
 #define CTRL_C  LCTL(KC_C)
 
 #define CAPSWD  CAPS_WORD
-#define TD_RSH  TD(DANCE_0)
+#define RU_TDSH  TD(DANCE_0)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    L_NUM,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_GRV,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_UNDS, KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
@@ -67,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, RU_EF,   RU_YERU, RU_VE,   RU_A,    RU_PE,                              RU_ER,   RU_O,    RU_EL,   RU_DE,   RU_ZHE,  RU_E,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, RU_YA,   RU_CHE,  RU_ES,   RU_EM,   RU_I,    _______,          _______, RU_TE,   TD_RSH,  RU_BE,   RU_YU,   RU_DOT,  RU_COLN,
+     _______, RU_YA,   RU_CHE,  RU_ES,   RU_EM,   RU_I,    _______,          _______, RU_TE,   RU_TDSH, RU_BE,   RU_YU,   RU_DOT,  RU_COLN,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -78,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_WH_U, CTRL_W,  KC_MS_U, CAPSWD,  KC_BTN2,                            KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
+     _______, KC_WH_U, CTRL_W,  KC_MS_U, KC_PSCR, KC_BTN2,                            KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1,                            KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, XXXXXXX,
+     _______, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1,                            KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_CAPS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, CTRL_Z,  L_SYM,   KC_LALT, KC_LCTL, CTRL_V,  KC_BTN3,          _______, CTRL_C,  KC_RCTL, KC_LALT, L_SYM,   KC_APP,  KC_PSCR,
+     _______, CTRL_Z,  L_SYM,   KC_LALT, KC_LCTL, CTRL_V,  KC_BTN3,          _______, CTRL_C,  KC_RCTL, KC_LALT, L_SYM,   KC_APP,  CAPSWD,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -92,7 +94,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_MINS, KC_F12,
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_MINS, KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, ST_M_0,  KC_LABK, KC_LCBR, KC_LPRN, KC_LBRC,                            ST_M_2,  KC_COLN, KC_SCLN, KC_PIPE, KC_PLUS, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -100,17 +102,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_NUM] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX,                            KC_PPLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PSLS,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,                            KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_PAST, KC_EQL,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, CTRL_Z,  XXXXXXX, KC_LALT, KC_LCTL, CTRL_V,  _______,          _______, CTRL_C,  KC_P1,   KC_P2,   KC_P3,   KC_PCMM, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, KC_PENT, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
 
-const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BACKSPACE, KC_DEL);
+// const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BACKSPACE, KC_DEL);
 const key_override_t double_quote_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOUBLE_QUOTE, KC_QUOTE);
 const key_override_t underscore_override = ko_make_basic(MOD_MASK_SHIFT, KC_UNDERSCORE, KC_MINUS);
 const key_override_t colon_override = ko_make_basic(MOD_MASK_SHIFT, KC_COLON, KC_SEMICOLON);
 
 // This globally defines all key overrides to be used
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &delete_key_override,
     &double_quote_override,
     &underscore_override,
     &colon_override,
@@ -164,6 +179,42 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       break;
   }
   return true;
+}
+
+// changes underglow based on current layer
+#define RBG_VAL 120
+layer_state_t layer_state_set_user(layer_state_t state) {
+  enum iris_layers {
+      _COLEMAK,
+      _RUS,
+      _NAV,
+      _SYM,
+      _NUM,
+  };
+
+  switch(biton32(state)) {
+    case _RUS:
+      // green-ish
+      rgblight_sethsv_noeeprom(72, 221, RBG_VAL);
+      break;
+    case _NAV:
+      // Red
+      rgblight_sethsv_noeeprom(0, 255, RBG_VAL);
+      break;
+    case _SYM:
+      // Dark Blue
+      rgblight_sethsv_noeeprom(255, 0, RBG_VAL);
+      break;
+    case _NUM:
+      // Violet
+      rgblight_sethsv_noeeprom(96, 96, RBG_VAL);
+      break;
+    default:
+      // Default colors
+      rgblight_sethsv(32, 170, RBG_VAL);
+      break;
+  }
+  return state;
 }
 
 typedef struct {
--- moonlander/config.h
+++ moonlander/config.h
@@ -7,25 +7,24 @@
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
-#undef TAPPING_TERM
-#define TAPPING_TERM 200
-
-// Make X and > to be easier triggered
-//#define TAPPING_TERM_PER_KEY
-
 // Apply the modifier on keys that are tapped during a short hold of a modtap
 #define PERMISSIVE_HOLD
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 200
+
+// Make X and > to be easier triggered
+#define TAPPING_TERM_PER_KEY
+
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 100
 
-#define FIRMWARE_VERSION u8"gqXJ4/gEdpq"
+#define FIRMWARE_VERSION u8"gqXJ4/4Xqlo"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
 #define RGB_MATRIX_STARTUP_SPD 60
-
