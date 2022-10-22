2719ae871fa3c856c32547275b20dccaa675937c
Nick Linker <nlinker@gmail.com>
2022-10-23 02:46:57 +0600

    adjust nav and num

--- moonlander/keymap.c
+++ moonlander/keymap.c
@@ -67,17 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_NAV] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TG(6),                  TG(7),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, LCTL(KC_Q),     LCTL(KC_W),     LCTL(KC_F),     LCTL(KC_D),     LCTL(KC_Y),     KC_TRANSPARENT,         KC_TRANSPARENT, LCTL(KC_C),     KC_HOME,        KC_UP,          KC_END,         KC_PGUP,        KC_TRANSPARENT,
-    KC_TRANSPARENT, MO(5),          KC_LGUI,        KC_LALT,        KC_LCTRL,       KC_APPLICATION, KC_TRANSPARENT,         KC_TRANSPARENT, LCTL(KC_V),     KC_LEFT,        KC_DOWN,        KC_RIGHT,       MO(5),          KC_TRANSPARENT,
-    A_ATAB,         LCTL(KC_Z),     KC_NO,          KC_NO,          KC_LSHIFT,      KC_PSCREEN,                                             KC_DELETE,      KC_INSERT,      LCTL(KC_LBRACKET),LCTL(KC_RBRACKET),KC_PGDOWN,  L_LOCK,
+    KC_TRANSPARENT, LCTL(KC_Q),     LCTL(KC_W),     LCTL(KC_F),     LCTL(KC_P),     LCTL(KC_B),     KC_TRANSPARENT,         KC_TRANSPARENT, LCTL(KC_C),     KC_HOME,        KC_UP,          KC_END,         KC_PGUP,        KC_TRANSPARENT,
+    KC_TRANSPARENT, MO(5),          KC_LALT,        KC_LCTRL,       KC_LSHIFT,      KC_LGUI,        KC_TRANSPARENT,         KC_TRANSPARENT, LCTL(KC_V),     KC_LEFT,        KC_DOWN,        KC_RIGHT,       MO(5),          KC_TRANSPARENT,
+    A_ATAB,         LCTL(KC_Z),     KC_NO,          KC_NO,          LCTL(KC_D),     LCTL(KC_Y),                                             KC_DELETE,      KC_INSERT,      LCTL(KC_LBRACKET),LCTL(KC_RBRACKET),KC_PGDOWN,  L_LOCK,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [_NUM] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          LCTL(KC_C),     KC_TRANSPARENT,         KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,
-    KC_TRANSPARENT, MO(5),          KC_F5,          KC_F6,          KC_F7,          LCTL(KC_V),     KC_TRANSPARENT,         KC_TRANSPARENT, KC_0,           KC_RCTRL,       KC_LALT,        KC_RALT,        MO(5),          KC_TRANSPARENT,
-    A_ATAB,         KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,                                                 KC_6,           KC_RSHIFT,      KC_7,           KC_8,           KC_9,           L_LOCK,
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,         KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
+    KC_TRANSPARENT, MO(5),          KC_PSCREEN,     KC_F6,          KC_F7,          KC_APPLICATION, KC_TRANSPARENT,         KC_TRANSPARENT, KC_RALT,        KC_RSHIFT,      KC_RCTRL,       KC_LALT,        MO(5),          KC_TRANSPARENT,
+    A_ATAB,         KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,                                                 KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           L_LOCK,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
