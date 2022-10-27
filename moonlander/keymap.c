4effa196ea4573d01004176fe313ef11763f2b0e
Nick Linker <nlinker@gmail.com>
2022-10-27 08:05:09 +0600

    change the order of F-keys

--- moonlander/keymap.c
+++ moonlander/keymap.c
@@ -58,8 +58,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_NUM] = LAYOUT_moonlander(
     KC_ESCAPE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,         KC_TRANSPARENT, L_RUS,          KC_7,           KC_8,           KC_9,           KC_TRANSPARENT, KC_NO,
-    KC_TRANSPARENT, MO(5),          KC_PSCREEN,     KC_F6,          KC_F7,          KC_APPLICATION, KC_TRANSPARENT,         KC_TRANSPARENT, KC_RALT,        MT(MOD_RSFT, KC_4),MT(MOD_RCTL, KC_5),MT(MOD_LALT, KC_6),MO(5), KC_TRANSPARENT,
-    A_ATAB,         KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,                                                 KC_0,           KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, L_LOCK,
+    KC_TRANSPARENT, MO(5),          KC_PSCREEN,     KC_F11,         KC_F12,         KC_APPLICATION, KC_TRANSPARENT,         KC_TRANSPARENT, KC_RALT,        MT(MOD_RSFT, KC_4),MT(MOD_RCTL, KC_5),MT(MOD_LALT, KC_6),MO(5), KC_TRANSPARENT,
+    A_ATAB,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,                                                 KC_0,           KC_1,           KC_2,           KC_3,           KC_TRANSPARENT, L_LOCK,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                         KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
   ),
