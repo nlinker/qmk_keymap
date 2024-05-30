bcfe117cc5f7485936e09d1053781d235bd12ad3
Nick Linker <n.linker@bi.zone>
2024-05-30 17:31:00 +0600

    make buffer operations on Nav

--- moonlander/config.h
+++ moonlander/config.h
@@ -5,17 +5,21 @@
 #define ORYX_CONFIGURATOR
 #define PERMISSIVE_HOLD
 
+// When the user holds a key after tapping it, the tapping function is repeated by default,
+// rather than activating the hold function. This allows keeping the ability to auto-repeat
+// the tapping function of a dual-role key. QUICK_TAP_TERM enables fine tuning of that
+// ability. If set to 0, it will remove the auto-repeat ability and activate the hold function instead.
 #define QUICK_TAP_TERM 90
 
 #define USB_SUSPEND_WAKEUP_DELAY 0
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX 100
 
-#define FIRMWARE_VERSION u8"D5r7P/GagLV"
+#define FIRMWARE_VERSION u8"D5r7P/ALYPZ"
 #define RAW_USAGE_PAGE 0xFF60
 #define RAW_USAGE_ID 0x61
 #define LAYER_STATE_8BIT
-#define COMBO_COUNT 10
+#define COMBO_COUNT 11
 
 #define RGB_MATRIX_STARTUP_SPD 60
 #define COMBO_MUST_HOLD_MODS // if a combo triggers a modifier, only trigger when the combo is held
